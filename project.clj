33a48eca939cf700a6f160707a4e12da2ce2852a
Florian Fischer <ffischer@proficom.de>
2019-12-05 16:02:47 +0100

    Day1 finished

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
-  :dev-dependencies [[lein-fore-prob "0.1.3"]]
+  :dev-dependencies [[lein-fore-prob "0.1.3"]
+                     [pjstadig/humane-test-output "0.10.0"]]
   :plugins [[lein-fore-prob "0.1.3"]
             [lein-auto "0.1.3"]
             [lein-ancient "0.6.15"]
