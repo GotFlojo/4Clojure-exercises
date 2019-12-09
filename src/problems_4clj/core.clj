e13dad10f52825ad2b17f0816321d26d635ceaf8
Florian Fischer <ffischer@proficom.de>
2019-12-09 12:34:37 +0100

    Add week1 day4 exercices

--- src/problems_4clj/core.clj
+++ src/problems_4clj/core.clj
@@ -1,2 +1,39 @@
 (ns problems-4clj.core)
 
+
+
+;; problem 20 (Easy)
+(def penultimate-element-solution
+  (fn [& args] ;; update args as needed
+  ;; Write a function which returns the second to last element from a sequence.
+  nil))
+
+
+;; problem 24 (Easy)
+(def sum-it-all-up-solution
+  (fn [& args] ;; update args as needed
+  ;; Write a function which returns the sum of a sequence of numbers.
+  nil))
+
+
+;; problem 25 (Easy)
+(def find-the-odd-numbers-solution
+  (fn [& args] ;; update args as needed
+  ;; Write a function which returns only the odd numbers from a sequence.
+  nil))
+
+
+;; problem 27 (Easy)
+(def palindrome-detector-solution
+  (fn [& args] ;; update args as needed
+  ;; Write a function which returns true if the given sequence is a palindrome.
+  ;; 
+  ;; Hint: "racecar" does not equal '(\r \a \c \e \c \a \r)
+  nil))
+
+
+;; problem 32 (Easy)
+(def duplicate-a-sequence-solution
+  (fn [& args] ;; update args as needed
+  ;; Write a function which duplicates each element of a sequence.
+  nil))
--- test/problems_4clj/core_test.clj
+++ test/problems_4clj/core_test.clj
@@ -2,3 +2,44 @@
   (:require [clojure.test :refer :all]
             [problems-4clj.core :refer :all]))
 
+
+
+;; problem 20
+(deftest can-penultimate-element
+  (is (= (penultimate-element-solution (list 1 2 3 4 5)) 4))
+  (is (= (penultimate-element-solution ["a" "b" "c"]) "b"))
+  (is (= (penultimate-element-solution [[1 2] [3 4]]) [1 2])))
+
+
+; ;; problem 24
+; (deftest can-sum-it-all-up
+;   (is (= (sum-it-all-up-solution [1 2 3]) 6))
+;   (is (= (sum-it-all-up-solution (list 0 -2 5 5)) 8))
+;   (is (= (sum-it-all-up-solution #{4 2 1}) 7))
+;   (is (= (sum-it-all-up-solution '(0 0 -1)) -1))
+;   (is (= (sum-it-all-up-solution '(1 10 3)) 14)))
+
+
+; ;; problem 25
+; (deftest can-find-the-odd-numbers
+;   (is (= (find-the-odd-numbers-solution #{1 2 3 4 5}) '(1 3 5)))
+;   (is (= (find-the-odd-numbers-solution [4 2 1 6]) '(1)))
+;   (is (= (find-the-odd-numbers-solution [2 2 4 6]) '()))
+;   (is (= (find-the-odd-numbers-solution [1 1 1 3]) '(1 1 1 3))))
+
+
+; ;; problem 27
+; (deftest can-palindrome-detector
+;   (is (false? (palindrome-detector-solution '(1 2 3 4 5))))
+;   (is (true? (palindrome-detector-solution "racecar")))
+;   (is (true? (palindrome-detector-solution [:foo :bar :foo])))
+;   (is (true? (palindrome-detector-solution '(1 1 3 3 1 1))))
+;   (is (false? (palindrome-detector-solution '(:a :b :c)))))
+
+
+; ;; problem 32
+; (deftest can-duplicate-a-sequence
+;   (is (= (duplicate-a-sequence-solution [1 2 3]) '(1 1 2 2 3 3)))
+;   (is (= (duplicate-a-sequence-solution [:a :a :b :b]) '(:a :a :a :a :b :b :b :b)))
+;   (is (= (duplicate-a-sequence-solution [[1 2] [3 4]]) '([1 2] [1 2] [3 4] [3 4])))
+;   (is (= (duplicate-a-sequence-solution [[1 2] [3 4]]) '([1 2] [1 2] [3 4] [3 4]))))
