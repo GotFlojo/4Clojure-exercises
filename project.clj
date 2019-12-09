60286a49c27ea0eb7a513e5739e815a739b3617f
Florian Fischer <ffischer@proficom.de>
2019-12-09 12:30:41 +0100

    Solutions Week1 Day3

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://www.4clojure.com/"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [pjstadig/humane-test-output "0.10.0"]]
+  :injections [(require 'pjstadig.humane-test-output)
+               (pjstadig.humane-test-output/activate!)]
   :dev-dependencies [[lein-fore-prob "0.1.3"]]
   :plugins [[lein-fore-prob "0.1.3"]
             [lein-auto "0.1.3"]
--- test/problems_4clj/core_test.clj
+++ test/problems_4clj/core_test.clj
@@ -5,6 +5,49 @@
 
 
 
-;; problem 3
-(deftest can-intro-to-strings
-  (is (= intro-to-strings-solution (.toUpperCase "hello world"))))
+
+;; problem 37
+(deftest can-regular-expressions
+  (is (= regular-expressions-solution (apply str (re-seq #"[A-Z]+" "bA1B3Ce ")))))
+
+
+;; ;; problem 57
+(deftest can-simple-recursion
+  (is (= simple-recursion-solution ((fn foo [x] (when (> x 0) (conj (foo (dec x)) x))) 5))))
+
+
+;; ;; problem 68
+(deftest can-recurring-theme
+  (is (= recurring-theme-solution
+  (loop [x 5
+         result []]
+    (if (> x 0)
+      (recur (dec x) (conj result (+ 2 x)))
+      result)))))
+
+
+;; ;; problem 71
+(deftest can-rearranging-code->
+  (is (= (rearranging-code->-solution (sort (rest (reverse [2 5 4 1 3 6]))))
+   (-> [2 5 4 1 3 6] (reverse) (rest) (sort) (rearranging-code->-solution))
+   5)))
+
+
+;; ;; problem 72
+(deftest can-rearranging-code->>
+  (is (= (rearranging-code->>-solution (map inc (take 3 (drop 2 [2 5 4 1 3 6]))))
+   (->> [2 5 4 1 3 6] (drop 2) (take 3) (map inc) (rearranging-code->>-solution))
+   11)))
+
+
+;; ;; problem 145
+(deftest can-for-the-win
+  (is (= for-the-win-solution (for [x (range 40)
+            :when (= 1 (rem x 4))]
+        x)))
+  (is (= for-the-win-solution (for [x (iterate #(+ 4 %) 0)
+            :let [z (inc x)]
+            :while (< z 40)]
+        z)))
+  (is (= for-the-win-solution (for [[x y] (partition 2 (range 20))]
+        (+ x y)))))
