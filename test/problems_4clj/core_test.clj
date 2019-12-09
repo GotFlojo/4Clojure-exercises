63d170686aab8c1f869589e8888a4b2463621005
Florian Fischer <ffischer@proficom.de>
2019-12-09 12:31:34 +0100

    Cleanup Master

--- test/problems_4clj/core_test.clj
+++ test/problems_4clj/core_test.clj
@@ -2,9 +2,3 @@
   (:require [clojure.test :refer :all]
             [problems-4clj.core :refer :all]))
 
-
-
-
-;; problem 3
-(deftest can-intro-to-strings
-  (is (= intro-to-strings-solution (.toUpperCase "hello world"))))
